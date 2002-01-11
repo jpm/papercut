4f7e00e1291dd83bcbcfa1b98c9daa34c07a43f0
Joao Prado Maia <jpm@pessoal.org>
2002-01-11 01:51:15 +0000

    Adding a .cvsignore file to the 'logs' directory

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # Copyright (c) 2001 Joao Prado Maia. See the LICENSE file for more information.
-# $Id: settings.py,v 1.2 2002-01-11 01:51:15 jpm Exp $
+# $Id: settings.py,v 1.3 2002-01-11 01:52:38 jpm Exp $
 import time
 
 log_path = "/home/jpm/papercut/logs/"
@@ -8,8 +8,8 @@ log_file = log_path + "papercut.log"
 hostname = 'phpbrasil.com'
 backend_type = "mysql"
 dbname = "phpbrasil_dev"
-dbuser = "jpm"
-dbpass = "adivinhe"
+dbuser = "anonimo"
+dbpass = "anonimo"
 
 def logEvent(msg):
     f = open(log_file, "a")
