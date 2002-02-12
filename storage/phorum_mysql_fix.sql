3185eb755e4c24ba2755545625ea1f7165141d0a
Joao Prado Maia <jpm@pessoal.org>
2002-02-12 17:03:23 +0000

    Documenting the installation procedure

---
+++ storage/phorum_mysql_fix.sql
@@ -0,0 +1,14 @@
+#
+# Please change the values here as appropriate to your 
+# setup (i.e. table name or size of the 'nntp_group_name' field)
+#
+# Warning: Do not change the field name to something else than 'nttp_group_name'!
+#
+ALTER TABLE forums ADD nntp_group_name VARCHAR(30) AFTER id;
+ALTER TABLE forums ADD UNIQUE (nntp_group_name); 
+
+#
+# After dumping this file into MySQL you will need to manually update the contents
+# of the 'nttp_group_name' to associate a table name / forum with a newsgroup to
+# be available on Papercut
+#
