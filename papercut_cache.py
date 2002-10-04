4d878b5fb5167dc1dee8530101294cb3ad80eb9f
Joao Prado Maia <jpm@pessoal.org>
2002-10-04 03:04:12 +0000

    Only caching some of the methods

--- papercut_cache.py
+++ papercut_cache.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # Copyright (c) 2002 Joao Prado Maia. See the LICENSE file for more information.
-# $Id: papercut_cache.py,v 1.5 2002-10-04 02:41:35 jpm Exp $
+# $Id: papercut_cache.py,v 1.6 2002-10-04 03:04:12 jpm Exp $
 
 import binascii
 import md5
@@ -22,25 +22,29 @@ cache_methods = ('get_XHDR', 'get_XGTITLE', 'get_LISTGROUP',
 class CallableWrapper:
     name = None
     thecallable = None
+    cacheable_methods = ()
 
-    def __init__(self, name, thecallable):
+    def __init__(self, name, thecallable, cacheable_methods):
         self.name = name
         self.thecallable = thecallable
+        self.cacheable_methods = cacheable_methods
 
     def __call__(self, *args, **kwds):
-        filename = self._get_filename(*args, **kwds)
-        if os.path.exists(filename):
-            # check the expiration
-            expire, result = self._get_cached_result(filename)
-            diff = time.time() - expire
-            if diff > settings.nntp_cache_expire:
-                # remove the file and run the method again
-                return self._save_result(filename, *args, **kwds)
-            else:
-                return result
+        if self.name not in self.cacheable_methods:
+            return self.thecallable(*args, **kwds)
         else:
-            return self._save_result(filename, *args, **kwds)
-        return 
+            filename = self._get_filename(*args, **kwds)
+            if os.path.exists(filename):
+                # check the expiration
+                expire, result = self._get_cached_result(filename)
+                diff = time.time() - expire
+                if diff > settings.nntp_cache_expire:
+                    # remove the file and run the method again
+                    return self._save_result(filename, *args, **kwds)
+                else:
+                    return result
+            else:
+                return self._save_result(filename, *args, **kwds) 
 
     def _get_cached_result(self, filename):
         inf = open(filename, 'rb')
@@ -82,9 +86,6 @@ class Cache:
     def __getattr__(self, name):
         result = getattr(self.backend, name)
         if callable(result):
-            if name not in cacheable_methods:
-                result = result()
-            else:
-                result = CallableWrapper(name, result)
-            return result
+            result = CallableWrapper(name, result, self.cacheable_methods)
+        return result
 
